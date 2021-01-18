0379cab48c32ef87c9f3ae35697e8773679c6fb4
octopuscart <dev@develop.com>
2021-01-18 20:45:06 +0530

    cahgns

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -221,7 +221,7 @@ class Api extends REST_Controller {
         }
     }
 
-    function crateLocation_post() {
+    function createLocation_post() {
         $this->config->load('rest', TRUE);
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
